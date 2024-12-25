32bc87269863a68f95d1a58a64970f2e25ce0f8f
pavel mansilla goyas <pavel.mansilla@gmail.com>
2024-12-25 11:23:02 +0000

    fixed render-build for api

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,12 +3,10 @@
 set -o errexit
 
 bundle install
-bundle exec ./bin/rails assets:precompile
-bundle exec ./bin/rails assets:clean
 
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
 
-bundle exec ./bin/rails db:migrate
-bundle exec ./bin/rails db:seed
+bundle exec rails db:migrate
+bundle exec rails db:seed
